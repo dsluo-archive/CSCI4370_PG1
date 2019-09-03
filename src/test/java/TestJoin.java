ce64a6ed4b522da9b922feb6a9b7ca288b825bd1
David Luo <me@dsluo.dev>
2019-09-03 10:37:40 -0400

    basic tests

--- src/test/java/TestJoin.java
+++ src/test/java/TestJoin.java
@@ -35,6 +35,18 @@ public class TestJoin {
 
         equijoin = new Table("equijoin", "title year length genre studioName producerNo name address presNo",
                 "String Integer Integer String String Integer String String Integer", "title year");
+        var join0 = new Comparable[]{"Star_Wars", 1977, 124, "sciFi", "Fox", 12345, "Fox", "Los_Angeles", 7777};
+        var join1 = new Comparable[]{"Star_Wars_2", 1980, 124, "sciFi", "Fox", 12345, "Fox", "Los_Angeles", 7777};
+        var join2 = new Comparable[]{"Rocky", 1985, 200, "action", "Universal", 12125, "Universal", "Universal_City", 8888};
+        var join3 = new Comparable[]{"Rambo", 1978, 100, "action", "Universal", 32355, "Universal", "Universal_City", 8888};
+        equijoin.insert(join0);
+        equijoin.insert(join1);
+        equijoin.insert(join2);
+        equijoin.insert(join3);
+
+        natrualJoin = new Table("naturalJoin", "title year length genre studioName producerNo name address presNo",
+                "String Integer Integer String String Integer String String Integer", "title year");
+
     }
 
     @After
@@ -45,20 +57,25 @@ public class TestJoin {
 
     @Test
     public void nestedLoopJoin() {
+        var joined = movie.join("studioName", "name", studio);
+        assertEquals(equijoin, joined);
     }
 
     @Test
     public void indexedJoin() {
-
+        var joined = movie.i_join("studioName", "name", studio);
+        assertEquals(equijoin, joined);
     }
 
     @Test
     public void hashedJoin() {
-
+        var joined = movie.h_join("studioName", "name", studio);
+        assertEquals(equijoin, joined);
     }
 
     @Test
-    public void natrualJoin() {
-
+    public void naturalJoin() {
+        var joined = movie.join(studio);
+        assertEquals(natrualJoin, joined);
     }
 }
