6bc85c16e6e25c6c706f180011529addc6c96d43
David Luo <me@dsluo.dev>
2019-09-04 14:43:11 -0400

    correct natrualjoin test

--- src/test/java/TestJoin.java
+++ src/test/java/TestJoin.java
@@ -1,15 +1,18 @@
-import static org.junit.Assert.assertEquals;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestJoin {
 
     private Table movie;
     private Table studio;
     private Table equijoin;
-    private Table natrualJoin;
+
+    private Table student;
+    private Table ta;
+    private Table naturalJoin;
 
     @Before
     public void setUp() {
@@ -44,8 +47,28 @@ public class TestJoin {
         equijoin.insert(join2);
         equijoin.insert(join3);
 
-        natrualJoin = new Table("naturalJoin", "title year length genre studioName producerNo name address presNo",
-                "String Integer Integer String String Integer String String Integer", "title year");
+        student = new Table("student", "name id year gpa", "String Integer Integer Float", "id");
+        var student0 = new Comparable[]{"Fred", 0, "Freshman", 3.5};
+        var student1 = new Comparable[]{"Sally", 1, "Sophomore", 2.8};
+        var student2 = new Comparable[]{"Susan", 2, "Graduate", 3.9};
+        var student3 = new Comparable[]{"Robert", 3, "Senior", 3.1};
+        student.insert(student0);
+        student.insert(student1);
+        student.insert(student2);
+        student.insert(student3);
+
+        ta = new Table("ta", "name id course", "String Integer String", "id");
+        var ta0 = new Comparable[]{"Susan", 2, "Underwater Basket Weaving"};
+        var ta1 = new Comparable[]{"Robert", 3, "Explosive Demolition 101"};
+        ta.insert(ta0);
+        ta.insert(ta1);
+
+        naturalJoin = new Table("naturalJoin", "name id year gpa course",
+                "String Integer Integer Float String", "id");
+        var natJoin0 = new Comparable[]{"Susan", 2, "Graduate", 3.9, "Underwater Basket Weaving"};
+        var natJoin1 = new Comparable[]{"Robert", 3, "Senior", 3.1, "Explosive Demolition 101"};
+        naturalJoin.insert(natJoin0);
+        naturalJoin.insert(natJoin1);
 
     }
 
@@ -58,24 +81,24 @@ public class TestJoin {
     @Test
     public void nestedLoopJoin() {
         var joined = movie.join("studioName", "name", studio);
-        assertEquals(equijoin, joined);
+        assertTrue(equijoin.equalsIgnoreName(joined));
     }
 
     @Test
     public void indexedJoin() {
         var joined = movie.i_join("studioName", "name", studio);
-        assertEquals(equijoin, joined);
+        assertTrue(equijoin.equalsIgnoreName(joined));
     }
 
     @Test
     public void hashedJoin() {
         var joined = movie.h_join("studioName", "name", studio);
-        assertEquals(equijoin, joined);
+        assertTrue(equijoin.equalsIgnoreName(joined));
     }
 
     @Test
     public void naturalJoin() {
-        var joined = movie.join(studio);
-        assertEquals(natrualJoin, joined);
+        var joined = student.join(ta);
+        assertTrue(naturalJoin.equalsIgnoreName(joined));
     }
 }
