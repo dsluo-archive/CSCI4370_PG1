165ba097175c5f8b2a8ac987f6cafa675ee821e9
David Luo <me@dsluo.dev>
2019-09-04 16:57:34 -0400

    author

--- src/test/java/TestJoin.java
+++ src/test/java/TestJoin.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * @author David Luo
+ */
 public class TestJoin {
-
-
     private Table student;
     private Table ta;
 
